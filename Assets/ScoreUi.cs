dcf76bce6f5db1fa84b179528e4ec230d7ae070a
Eran Elbaz <eranelbaz97@gmail.com>
2020-10-31 18:14:09 +0200

    add source code

---
+++ Assets/ScoreUi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ScoreUi : MonoBehaviour
+{
+    public RowUi rowUi;
+    public ScoreManager scoreManager;
+    
+    void Start()
+    {
+        var scores = scoreManager.GetHighScores().ToArray();
+        for (int i = 0; i < scores.Length; i++)
+        {
+            var row = Instantiate(rowUi, transform).GetComponent<RowUi>();
+            row.rank.text = (i + 1).ToString();
+            row.name.text = scores[i].name;
+            row.score.text = scores[i].score.ToString();
+        }
+    }
+}
+
